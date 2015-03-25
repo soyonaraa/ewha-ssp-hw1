95f575c10d01b61677764c63f9e74539171ac0bb
narangerelt <soyonaraa@gmail.com>
2015-03-25 22:40:43 +0900

    Updated line 8 and 9, filled setup() and loop()

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -5,20 +5,28 @@
  * GitHub Setup Homework 
  * Instructor: Prof. HyungJune Lee
  *
- * Coded by Firstname Lastname
- * Mar xx, 2015
+ * Coded by Narangerelt Batsoyol
+ * Mar 25, 2015
  *
  */
 
-// Add #define pre-processor below
 
+#define FRONT_LED_PIN 10
+#define REAR_LED_PIN 9
  
 void setup() {
-  //Add your code of Lab Assignment #1
+  pinMode(FRONT_LED_PIN, OUTPUT);
+  pinMode(REAR_LED_PIN, OUTPUT);
 
 }
 
 void loop() {
-  //Add your code of Lab Assignment #1  
+  digitalWrite(REAR_LED_PIN, LOW);
+  digitalWrite(FRONT_LED_PIN, HIGH);
+  delay(1000);
+
+  digitalWrite(FRONT_LED_PIN, LOW);
+  digitalWrite(REAR_LED_PIN, HIGH);
+  delay(1000);  
 
 }
